a55009b47f8652a88fa790dcc32b00234eab63b7
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-11-23 14:08:25 +0530

    Time: 20 ms (98.21%), Space: 21.6 MB (89.29%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,18 +1,46 @@
 class Solution {
+private:
+        bool isArithmetic(vector<int>& nums, int l, int r) {
+        int maxVal = INT_MIN, minVal = INT_MAX;
+
+        for (int i = l; i <= r; i++) {
+            maxVal = max(nums[i], maxVal);
+            minVal = min(nums[i], minVal);
+        }
+
+        int len = r - l + 1;
+        if ((maxVal - minVal) % (len - 1) != 0) {
+            return false;
+        }
+
+        int diff = (maxVal - minVal) / (len - 1);
+        if (diff == 0) {
+            return true;
+        }
+
+        vector<bool> visited(len, false);
+
+        for (int i = l; i <= r; i++) {
+            int val = nums[i];
+            if ((val - minVal) % diff != 0) {
+                return false;
+            } else {
+                int pos = (val - minVal) / diff;
+                if (visited[pos]) {
+                    return false;
+                }
+                visited[pos] = true;
+            }
+        }
+        return true;
+    }
+    
 public:
     vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-        vector<bool> ans;
-        for (auto i = 0, j = 0; i < l.size(); i++) {
-            vector<int> temp(begin(nums) + l[i], begin(nums) + r[i] + 1);
-            sort(temp.begin(), temp.end());
-            for (j = 2; j < temp.size(); j++) {
-                if (temp[j] - temp[j - 1] != temp[1] - temp[0])
-                    break;
-            }
-            
-            ans.push_back(j == temp.size());
+        vector<bool> result;
+        for (int i = 0; i < l.size(); i++) {
+            result.push_back(isArithmetic(nums, l[i], r[i]));
         }
-        
-        return ans;
+        return result;
     }
 };
