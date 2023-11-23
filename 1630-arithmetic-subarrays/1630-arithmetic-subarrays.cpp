9ab4fcd7630a571efe5083d300be550462816d45
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-11-23 14:04:12 +0530

    Time: 43 ms (81.66%), Space: 26.3 MB (69.48%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,37 +1,18 @@
 class Solution {
-private:
-    bool isAP(vector<int>& temp) {
-        int n = temp.size();
-        if(n <= 2) return true;
-        
-        int diff = temp[1] - temp[0];
-        for(int i = 1; i < n; i++) {
-            if(temp[i] - temp[i-1] != diff)
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool solve(vector<int>& nums, int left, int right) {
-        vector<int> temp (nums.begin() + left, nums.begin() + right + 1);
-        sort(temp.begin(), temp.end());
-        if(isAP(temp) == false) return false;
-        return true;
-    }
-    
 public:
     vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-        int m = l.size();
-        vector<bool> answer;
-        for(int i = 0; i < m; i++) {
-            if(solve(nums, l[i], r[i]) == false) {
-                answer.push_back(false);
+        vector<bool> ans;
+        for (auto i = 0, j = 0; i < l.size(); i++) {
+            vector<int> temp(begin(nums) + l[i], begin(nums) + r[i] + 1);
+            sort(temp.begin(), temp.end());
+            for (j = 2; j < temp.size(); j++) {
+                if (temp[j] - temp[j - 1] != temp[1] - temp[0])
+                    break;
             }
             
-            else answer.push_back(true);
+            ans.push_back(j == temp.size());
         }
         
-        return answer;
+        return ans;
     }
 };
