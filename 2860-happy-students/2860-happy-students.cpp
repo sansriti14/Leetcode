88810aa223d76e67a33474411efd6a8963d3e9ff
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-11-24 22:42:11 +0530

    Time: 99 ms (21.75%), Space: 72 MB (95.41%) - LeetHub

--- 2860-happy-students/2860-happy-students.cpp
+++ 2860-happy-students/2860-happy-students.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int countWays(vector<int>& nums) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        int ans = 0, n = nums.size();
-        int selected = 0;
+        int ans = 0, selected = 0;
+        
         if(nums[0] != 0) ans = 1; 
+        
         for(int i = 0; i < n; i++) {
             selected++;
             if(selected > nums[i]) {  
-                if(i+1 < n && selected<nums[i+1]) 
+                if(i+1 < n && selected < nums[i+1]) 
                     ans++; 
                 else if(i+1 == n) ans++; 
             }
