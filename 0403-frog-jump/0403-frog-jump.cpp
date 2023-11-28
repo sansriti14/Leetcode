62b187292717d7e621ef65524a76f33131a2d590
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-11-29 02:12:17 +0530

    Time: 120 ms (57.51%), Space: 210.8 MB (31.94%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -4,21 +4,17 @@ private:
         int n = stones.size();
         
         if(curr_index == n-1) return true;
-        if(curr_index >= n) return false;
         
         if(dp[curr_index][last_jump] != -1) return dp[curr_index][last_jump];
         
         bool ans = false;
-        
-        for(int i = -1; i < 2; i++) {
-            int next_jump = last_jump + i;
+        for(int next_index = curr_index+1; next_index < n; next_index++) {
+            if(stones[next_index] - stones[curr_index] > last_jump + 1) break;
             
-            if(next_jump > 0) {
-                int next_index = lower_bound(stones.begin(), stones.end(), stones[curr_index] + next_jump) - stones.begin();
-                if(next_index >= n || stones[next_index] != stones[curr_index] + next_jump) 
-                    continue;
-
-                ans = solve(next_index, next_jump, stones, dp) || ans;
+            for(int i = -1; i < 2; i++) {
+                if(stones[next_index] - stones[curr_index] == last_jump + i) {
+                    ans = solve(next_index, last_jump + i, stones, dp) || ans;
+                }
             }
         }
         
@@ -31,6 +27,6 @@ public:
         
         int n = stones.size();
         vector<vector<int>> dp (n, vector<int> (n+1, -1));
-        return solve(0, 0, stones, dp);
+        return solve(1, 1, stones, dp);
     }
 };
