892c044242b40c8b1a90d9353841bcb01e246614
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-12-04 21:42:06 +0530

    Time: 0 ms (100.00%), Space: 7 MB (18.84%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        int result = -1;
-        for (int i = 0; i + 2 < num.length(); i++) {
-            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
-                result = std::max(result, num[i] - '0');
-            }
+        vector<string> temp = {"999","888","777","666","555","444","333","222","111","000"};
+        for(auto it : temp) {
+            if(num.find(it) != -1)
+            return it;
         }
-        return (result == -1) ? "" : std::string(3, '0' + result);
+        
+        return "";
     }
 };
