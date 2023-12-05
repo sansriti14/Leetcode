7f45f863e15599265044f21f14448d54d9e2a8d7
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-12-05 23:18:13 +0530

    Time: 3 ms (17.67%), Space: 6.4 MB (13.68%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        return n - 1;
+        int count = 0;
+        int rev = 0;
+        while(n > 1) {
+            rev = n / 2;
+            count += rev;
+            n = n - rev;
+        }
+        return count;
     }
 };
+   
