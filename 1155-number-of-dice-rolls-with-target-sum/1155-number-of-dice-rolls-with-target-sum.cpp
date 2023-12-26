2e937d3d53690e923aa2dcbb1c9dbf73d7389fe6
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-12-26 22:41:35 +0530

    Time: 74 ms (12.56%), Space: 8.8 MB (65.29%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,22 +1,19 @@
 class Solution {   
-private:
-    int findWays(int n, int k, int target, vector<vector<int>>& dp) {
-        if(n == 0 && target == 0) return 1;
-        if(n <= 0 || target <= 0) return 0;
-        
-        if(dp[n][target] != -1) return dp[n][target];
-
-        int numWays = 0, mod = 1e9 + 7;
-        for(int i = 1; i <= k; i++) {
-                numWays = (numWays % mod + (findWays(n-1, k, target-i, dp)) % mod) % mod;
-        }
-
-        return dp[n][target] = numWays;
-    }
-    
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
-        return findWays(n, k, target, dp);
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        dp[0][0] = 1;
+        
+        int mod = 1e9 + 7;
+        for(int i = 1; i <= n; i++) {
+            for(int currSum = 1; currSum <= target; currSum++) {
+                for(int num = 1; num <= k; num++) {
+                    if(num <= currSum)
+                        dp[i][currSum] = (dp[i][currSum] + dp[i-1][currSum - num]) % mod;
+                }
+            }
+        }
+        
+        return dp[n][target];
     }
 };
