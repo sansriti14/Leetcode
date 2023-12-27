6c66849c9b6acce93bede808a3fee1c1772239a7
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-12-27 19:28:26 +0530

    Time: 132 ms (21.91%), Space: 109.4 MB (7.60%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,15 +1,23 @@
 class Solution {
+private:
+    int findMinTime(string& colors, vector<int>& neededTime, int index, char prev_color, int prev_time, vector<int>& dp) {
+        if(index < 0) return 0;
+        
+        if(dp[index] != -1) return dp[index];
+        
+        if(colors[index] == prev_color) {
+            return dp[index] = min(neededTime[index], prev_time) + findMinTime(colors, neededTime, index - 1, colors[index], max(neededTime[index], prev_time), dp);
+        }
+        
+        else {
+            return dp[index] = findMinTime(colors, neededTime, index - 1, colors[index], neededTime[index], dp);
+        }
+    }
+    
 public:
     int minCost(string colors, vector<int>& neededTime) {
         int n = colors.size();
-        int min_time = 0;
-        for (int i = 1; i < n; i++) {
-            if (colors[i] == colors[i - 1]) {
-                min_time += min(neededTime[i], neededTime[i - 1]);
-                neededTime[i] = max(neededTime[i], neededTime[i - 1]);
-            }
-        }
-        
-        return min_time;
+        vector<int> dp(n, -1);
+        return findMinTime(colors, neededTime, n-1, 'A', neededTime[n-1], dp);
     }
 };
