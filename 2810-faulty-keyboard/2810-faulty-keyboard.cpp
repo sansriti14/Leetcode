d60ddeaa2341910d6248483223c487be5370a2e5
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-12-27 20:39:24 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (63.23%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.cpp
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     string finalString(string s) {
-        int n = s.length();
-        for(int i = 0; i < n; i++) {
-            if(s[i] == 'i') {
-                int count = 0;
-                int j = i;
-                while(j < n && s[j] == 'i') {
-                    j++;
-                    count++;
-                }
-                
-                if(count % 2) reverse(s.begin(), s.begin() + i);
-                i = j;
-            }
-        }
-        
         string final_string = "";
-        for(int i = 0; i < n; i++) {
-            if(s[i] != 'i')
-                final_string += s[i];
+        
+        for(auto ch: s) {
+            if(ch != 'i')
+                final_string += ch;
+            else reverse(final_string.begin(), final_string.end());
         }
         
         return final_string;
