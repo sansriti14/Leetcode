0cefe172fae52bfe1f34a495ee519e1d6de3417a
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-12-28 13:39:30 +0530

    Time: 34 ms (82.02%), Space: 6.5 MB (100.00%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.cpp
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -1,59 +1,37 @@
 class Solution {
-    int n;
-    
-private:
-    int solve(int idx, char prev, int curCharFreqCount, string &s, int k, int dp[][27][11][101]) {
-        if(k < 0) return 1e9;
-        if(idx >= n) return 0;
-        
-        if(curCharFreqCount >= 10)
-            curCharFreqCount = 10;
-        
-        if(dp[idx][prev - 'a'][curCharFreqCount][k] != -1)
-            return dp[idx][prev - 'a'][curCharFreqCount][k];
-        
-        
-        int res = 1e9;
-        res = min(res, solve(idx+1, prev, curCharFreqCount, s, k-1, dp));
-        
-        if(s[idx] != prev)
-            res = min(res, 1 + solve(idx + 1, s[idx], 1, s, k, dp));
-        
-        else {
-            
-            if(curCharFreqCount == 1 || curCharFreqCount == 9)
-                res = min(res, 1 + solve(idx + 1, prev, (curCharFreqCount + 1), s, k, dp));
-            
-            else res = min(res, solve(idx + 1, prev, curCharFreqCount + 1, s, k, dp));
-        }
-        
-        return dp[idx][prev - 'a'][curCharFreqCount][k] = res;
-    }
-    
 public:
     int getLengthOfOptimalCompression(string s, int k) {
-        n = s.size();
-        if(n == k) return 0;
-        
-        bool allSame = true;
-        for(int i = 1; i < n; i++) {
-            if(s[i] != s[i-1]) {
-                allSame = false;
-                break;
+        int n = s.size();
+        int m = k;
+
+        int dp[110][110] = {};
+        for(int i = 1; i <= n; ++i) {
+            for(int j = 0; j <= i && j <= m; ++j) {
+                int need_remove = 0;
+                int group_count = 0;
+                dp[i][j] = INT_MAX;
+                if(j) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                
+                for (int k = i; k >= 1; --k) {
+                    if (s[k - 1] != s[i - 1]) need_remove += 1;
+                    else group_count += 1;
+                    
+                    if (need_remove > j) break;
+
+                    if (group_count == 1) 
+                        dp[i][j] = min(dp[i][j], dp[k - 1][j - need_remove] + 1);
+                    else if (group_count < 10) 
+                        dp[i][j] = min(dp[i][j], dp[k - 1][j - need_remove] + 2);
+                    else if (group_count < 100) 
+                        dp[i][j] = min(dp[i][j], dp[k - 1][j - need_remove] + 3);
+                    else
+                        dp[i][j] = min(dp[i][j], dp[k - 1][j - need_remove] + 4);
+                }
             }
         }
 
-        if(allSame) {
-            n  -= k;
-            if(n == 1) return 1;
-            
-            return log10(n) + 1 + 1;            
-        }
-        
-        
-        int dp[n+1][27][11][101];
-        
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 'z' + 1, 0, s, k, dp);
+        return dp[n][m];
     }
 };
