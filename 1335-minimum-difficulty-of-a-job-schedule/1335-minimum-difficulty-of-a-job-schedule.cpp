a3f95e86dff8ffbb929c9e6bc92098a43f0acda5
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2023-12-29 21:43:04 +0530

    Time: 111 ms (17.11%), Space: 8 MB (60.24%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,22 +1,4 @@
 class Solution {
-private:
-    int partition(vector<int>& jobDifficulty, int d, int index, vector<vector<int>>& dp) {
-        int n = jobDifficulty.size();
-        if(index == n && d == 0) return 0;
-        if(index == n || d == 0) return 1e9;
-        
-        if(dp[index][d] != -1) return dp[index][d];
-        
-        int curr_max_difficulty = 0;
-        int min_difficulty = 1e9;
-        for(int i = index; i < n; i++) {
-            curr_max_difficulty = max(curr_max_difficulty, jobDifficulty[i]);
-            min_difficulty = min(min_difficulty, curr_max_difficulty + partition(jobDifficulty, d - 1, i+1, dp));
-        }
-        
-        return dp[index][d] = min_difficulty;
-    }
-    
 public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n = jobDifficulty.size();
@@ -24,8 +6,24 @@ public:
         if(n < d) return -1;
         if(n == d) return accumulate(jobDifficulty.begin(), jobDifficulty.end(), 0);
         
-        vector<vector<int>> dp(n+1, vector<int>(d+1, -1));
+        vector<vector<int>> dp(d, vector<int>(n, 0));
+        dp[0][0] = jobDifficulty[0];
+        for(int j = 1; j < n; j++) {
+            dp[0][j] = max(dp[0][j-1], jobDifficulty[j]);
+        }
+
+        for(int i = 1; i < d; i++) {
+            for(int length = i; length < n; length++) {
+                int curr_max_difficulty = jobDifficulty[length];
+                dp[i][length] = 1e9;
+                
+                for(int schedule = length; schedule >= i; schedule--) {
+                    curr_max_difficulty = max(curr_max_difficulty, jobDifficulty[schedule]);
+                    dp[i][length] = min(dp[i][length], curr_max_difficulty + dp[i-1][schedule-1]);
+                }
+            }
+        }
         
-        return partition(jobDifficulty, d, 0, dp);
+        return dp[d-1][n-1];
     }
 };
