c782eec03c1dc3f4ff198f2f9a085be35c3b4d9d
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-02 15:13:32 +0530

    Time: 9 ms (5.12%), Space: 6.4 MB (26.83%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
-    int distinctIntegers(int n) 
-    {
-        if(n == 1) return 1;
-        return n - 1;
+    int distinctIntegers(int n) {
+        queue<int> q;
+        q.push(n);
+        int count = 1;
+        vector<bool> visited(n + 1);
+        
+        while(!q.empty()) {
+            int top = q.front();
+            q.pop();
+            
+            for(int i = 1; i < top; i++) {
+                if(top % i == 1 && !visited[i]) {
+                    visited[i]=true;
+                    count++;
+                    q.push(i);
+                }
+            }
+        }
+        
+        return count;
     }
 };
