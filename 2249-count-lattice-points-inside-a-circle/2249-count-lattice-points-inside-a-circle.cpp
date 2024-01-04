74c5a663feca3d5f7c10c01860fc57f6107e1157
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-04 22:20:28 +0530

    Time: 505 ms (71.37%), Space: 8.3 MB (86.26%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int countLatticePoints(vector<vector<int>>& circles) {
         int n = circles.size();
-        set<pair<int, int>> s;
         
-        for(int i = 0; i < n; i++) {
-            int a = circles[i][0], b = circles[i][1], radius = circles[i][2];
-            
-            for(int x = -radius; x <= radius; x++) {
-                for(int y = -radius; y <= radius; y++) {
-                    if((x * x) + (y * y) <= (radius * radius)) {
-                        s.insert({x + a, y + b});
+        int ans = 0;
+        for(int x = 0; x <= 200; x++) {
+            for(int y = 0; y <= 200; y++) {
+                for(int i = 0; i < n; i++) {
+                    int a = circles[i][0], b = circles[i][1], radius = circles[i][2];
+                    if((a - x) * (a - x) + (b - y) * (b - y) <= radius * radius){
+                        ans++;
+                        break;
                     }
                 }
             }
         }
-        
-        return s.size();
+        return ans;
     }
 };
