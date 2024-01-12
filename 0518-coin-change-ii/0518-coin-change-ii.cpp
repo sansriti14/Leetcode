f7c172ec919b51c0f0ace978fed8f1299aa76952
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-12 22:06:57 +0530

    Time: 35 ms (36.66%), Space: 19.1 MB (24.32%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,31 +1,34 @@
 class Solution {
-private:
-    int findNumCombinations(int amount, int n, vector<int>& coins, vector<vector<int>>& dp) {
-        if(n == 0) return 0;
-        if(amount == 0) return 1;
-        
-        if(dp[n][amount] != -1) return dp[n][amount];
-        
-        if(coins[n-1] > amount) {
-            dp[n][amount] = findNumCombinations(amount, n-1, coins, dp);
-            return dp[n][amount];
-        }
-        
-        int take = findNumCombinations(amount - coins[n-1], n, coins, dp);
-        int not_take = findNumCombinations(amount, n-1, coins, dp);
-        
-        return dp[n][amount] = take + not_take;
-    }
-    
 public:
+    
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        
         if(amount == 0) return 1;
-        
         if(n == 0) return 0;
         
-        vector<vector<int>> dp(n+2, vector<int> (amount+2, -1));
-        return findNumCombinations(amount, n, coins, dp);
+        vector<vector<int>> dp;
+        dp.resize(n+2, vector<int> (amount+2, -1));
+        
+        for(int i=0;i<n+1;i++) {
+            dp[i][0] = 1;
+        }
+        
+        for(int i=0;i<amount+1;i++) {
+            dp[0][i] = 0;
+        }
+        
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<amount+1;j++) {
+                if(coins[i-1] > j) {
+                    dp[i][j] = dp[i-1][j];
+                } 
+                
+                else {
+                    dp[i][j] = dp[i-1][j]+dp[i][j-coins[i-1]];
+                }
+            }
+        }
+        
+        return dp[n][amount];
     }
 };
