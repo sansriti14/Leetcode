5adad0a77748b0236710481158d477149e12f278
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-19 15:15:47 +0530

    Time: 13 ms (53.23%), Space: 13 MB (5.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,29 +1,32 @@
 class Solution {
 private:
-    int findMinPathSum(int curr_row, int curr_col, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
-        if(curr_row == n-1  && curr_col >= 0 && curr_col < n) return matrix[curr_row][curr_col];
+    int findMinSum(int curr_row, int curr_col, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+        int n = matrix.size();
+        
+        if(curr_row == n - 1 && curr_col >= 0 && curr_col < n) return matrix[curr_row][curr_col];
         if(curr_row == n || curr_col < 0 || curr_col == n) return 1e9;
         
-        if(dp[curr_row][curr_col] != INT_MAX) return dp[curr_row][curr_col];
+        if(dp[curr_row][curr_col] != 1e9) return dp[curr_row][curr_col];
         
-        int down = matrix[curr_row][curr_col] + findMinPathSum(curr_row + 1, curr_col, n, matrix, dp);
-        int diagonal_left = matrix[curr_row][curr_col] + findMinPathSum(curr_row + 1, curr_col - 1, n, matrix, dp);
-        int diagonal_right = matrix[curr_row][curr_col] + findMinPathSum(curr_row + 1, curr_col + 1, n, matrix, dp);
+        int down = matrix[curr_row][curr_col], diagonal_left = matrix[curr_row][curr_col], diagonal_right = matrix[curr_row][curr_col];
+        
+        down += findMinSum(curr_row + 1, curr_col, matrix, dp);
+        diagonal_left += findMinSum(curr_row + 1, curr_col - 1, matrix, dp);
+        diagonal_right += findMinSum(curr_row + 1, curr_col + 1, matrix, dp);
         
         return dp[curr_row][curr_col] = min(down, min(diagonal_left, diagonal_right));
-        
     }
     
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        vector<vector<int>> dp (n, vector<int> (n, 1e9));
         
-        int min_path_sum = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        int min_sum = 1e9;
         for(int col = 0; col < n; col++) {
-            min_path_sum = min(min_path_sum, findMinPathSum(0, col, n, matrix, dp));
+            min_sum = min(min_sum, findMinSum(0, col, matrix, dp));
         }
         
-        return min_path_sum;
+        return min_sum;
     }
 };
