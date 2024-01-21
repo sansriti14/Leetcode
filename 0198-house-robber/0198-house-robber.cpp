4eacb59fb11bb485a775de5f20e960c6717e2754
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-21 15:07:32 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,26 @@
 class Solution {
+private:
+    int findMaxAmount(int index, vector<int>& nums, vector<int>& dp) {
+        int n = nums.size();
+        
+        if(index >= n) return 0;
+        if(dp[index] != -1) return dp[index];
+        
+        int not_rob = findMaxAmount(index + 1, nums, dp);
+        int rob = max(not_rob, nums[index] + findMaxAmount(index + 2, nums, dp));
+        
+        return dp[index] = max(rob, not_rob);
+    }
+    
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
+        
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
         
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        vector<int> dp (n + 1, -1);
         
-        for(int i = 2; i < n; i++) {
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
-        }
-        return dp[n-1];
+        return findMaxAmount(0, nums, dp);
     }
 };
