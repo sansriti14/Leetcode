cdf27ef8553ede1a8296fa8db50a5eaa43067224
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-23 17:01:37 +0530

    Time: 78 ms (48.50%), Space: 88.3 MB (25.34%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    int maxLength(vector<string>& arr, int index = 0, string str = "") {
-        unordered_set<char> st (begin(str), end(str));
-        
-        if(st.size() != (int) str.length()) return 0;
-        
-        int curr_length = str.length();
-        for(int i = index; i < arr.size(); i++) {
-            string new_string = str + arr[i];
-            curr_length = max(curr_length, maxLength(arr, i, new_string));
+    bool check(string s,unordered_map<char,int> &map){
+        unordered_set<char> mp;
+        for(auto it:s){
+            if(mp.find(it)!=mp.end())return false;//found duplicate in the same string
+                mp.insert(it);
+            if(map[it]>0)return false;//found duplicate in the map and current string
         }
-        
-        return curr_length;
+        return true;
+    }
+    int solve(int i,vector<string> &arr,unordered_map<char,int> &map){
+        if(i>=arr.size())return 0;
+        int x=0;
+        x=solve(i+1,arr,map);//not pick
+        if(check(arr[i],map)){ //checking that it is possible to pick or not
+            for(auto it:arr[i])map[it]++; //putting in our map
+            x=max(x,(int)(solve(i+1,arr,map)+arr[i].size()));
+            for(auto it:arr[i]){//backtracking
+                map[it]--;
+            }
+        }
+        return x;
+    }
+    int maxLength(vector<string>& arr) {
+        unordered_map<char,int> map;
+        return solve(0,arr,map);
     }
 };
