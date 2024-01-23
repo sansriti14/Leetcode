f2459b989b6c0a556dc9ac8becbed0fad41842d1
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-23 16:39:16 +0530

    Time: 242 ms (21.82%), Space: 124.4 MB (18.82%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,6 +1,6 @@
 class Solution {    
 private:
-    bool hasUniqueChars(string& str) {
+    bool hasUniqueChars(string str) {
         unordered_set<char> st;
         for(auto ch: str) {
             if(st.find(ch) != st.end()) return false;
@@ -10,17 +10,13 @@ private:
         return true;
     }
     
-    int findMaxLength(int index, string& curr_str, vector<string>& arr) {
-        int n = arr.size();
-        if(index >= n) return 0;
+    int findMaxLength(int index, string curr_str, vector<string>& arr) {
+        if(index >= arr.size()) return 0;
         
         int not_pick = findMaxLength(index + 1, curr_str, arr);
         int pick = -1e9;
-        string new_string = curr_str + arr[index];
-        int length = arr[index].length();
-        
-        if(hasUniqueChars(new_string)) {
-            pick = max(not_pick, length + findMaxLength(index + 1, new_string, arr));
+        if(hasUniqueChars(curr_str + arr[index])) {
+            pick = max(not_pick, (int) arr[index].length() + findMaxLength(index + 1, curr_str + arr[index], arr));
         }
         
         return max(pick, not_pick);
@@ -28,8 +24,6 @@ private:
     
 public:
     int maxLength(vector<string>& arr) {
-        int n = arr.size();
-        string str = "";
-        return findMaxLength(0, str, arr);
+        return findMaxLength(0, "", arr);
     }
 };
