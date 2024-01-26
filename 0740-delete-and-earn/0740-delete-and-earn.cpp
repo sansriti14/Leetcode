72e35ac5217f10a55c95369de7c026d922dab8b8
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-26 19:08:22 +0530

    Time: 7 ms (86.70%), Space: 16.3 MB (30.30%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-    int dp[20001]; 
-    int findMaxPoints(int index, vector<int> &arr) {
-        int n = arr.size();
-        if(index >= n ) return 0;
-        if(dp[index] != -1) return dp[index];
+    int deleteAndEarn(vector<int>& nums) {
+        int n = 10001;
+        vector<int> dp(n);
         
-        int curr_value = arr[index];
-        int curr_sum = arr[index];
-        int i = index + 1;
-        
-        while(i < n && (arr[i] == curr_value || arr[i] == curr_value + 1)) {
-            curr_sum = curr_sum + ((arr[i] == curr_value) ? arr[i] : 0);
-            i++;
+        for(auto &it: nums) dp[it] += it;
+        for(int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], dp[i] + dp[i - 2]);
         }
-
-        return dp[index] = max(curr_sum + findMaxPoints(i, arr), findMaxPoints(index + 1, arr));
-    }
-    
-    int deleteAndEarn(vector<int>& arr) {
-        int n = arr.size(); 
-        memset(dp, -1, sizeof(dp));
-        sort(arr.begin(), arr.end());
-
-        return findMaxPoints(0, arr);
+        
+        return dp[n - 1];
     }
 };
