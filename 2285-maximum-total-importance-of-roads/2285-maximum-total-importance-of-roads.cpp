cf8d4b036d21b7db76f6bf1141b7bad2059c80f1
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-27 23:35:21 +0530

    Time: 413 ms (50.81%), Space: 151.1 MB (35.22%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        unordered_map<int, int> mp;
+        for(auto it: roads) {
+            mp[it[0]]++;
+            mp[it[1]]++;
+        }
+        
+        priority_queue<pair<int, int>> pq;
+        for(auto it: mp) pq.push({it.second, it.first});
+        
+        long long max_importance = 0;
+        while(!pq.empty()) {
+            auto [freq, city] = pq.top();
+            pq.pop();
+            
+            max_importance += ((long long)freq * (n--));
+        }
+        
+        return max_importance;
+    }
+};
