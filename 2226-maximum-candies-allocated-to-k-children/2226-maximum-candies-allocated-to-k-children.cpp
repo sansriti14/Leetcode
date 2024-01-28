9c943142d317ad4e3801035854ef011df0e6380e
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-29 02:41:47 +0530

    Time: 164 ms (42.74%), Space: 87.9 MB (5.12%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,14 +1,4 @@
-class Solution {
-private:
-    long long countChildren(int max_candies, vector<int>& candies) {
-        long long count = 0;
-        for(int i = 0; i < candies.size(); i++) {
-            count += (candies[i] / max_candies);
-        }
-        
-        return count;
-    }
-    
+class Solution {  
 public:
     int maximumCandies(vector<int>& candies, long long k) {
         sort(begin(candies), end(candies));
@@ -18,7 +8,13 @@ public:
         
         while(low < high) {
             int mid = (low + high + 1) / 2;
-            if(countChildren(mid, candies) >= k) low = mid;
+            
+            long long num_children = 0;
+            for(auto pile_size: candies) {
+                num_children += (pile_size / mid);
+            }
+            
+            if(num_children >= k) low = mid;
             else high = mid - 1;
         }
         
