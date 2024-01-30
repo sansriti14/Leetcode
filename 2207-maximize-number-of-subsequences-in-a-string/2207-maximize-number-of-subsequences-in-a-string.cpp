ec1426a1d6e11776eb8f49d112b88b1334d60702
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-30 20:24:42 +0530

    Time: 49 ms (73.65%), Space: 18.2 MB (48.73%) - LeetHub

--- 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.cpp
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.cpp
@@ -1,21 +1,20 @@
 class Solution {
-private:
-    long long count(string S1, string S2) {
-    int m = S1.size();
-    vector<long long> count(2, 0); 
-    for (int i = 0; i < m; i++) {
-        for (int j = 1; j >= 0; j--) {
-            if (S1[i] == S2[j]) {
-                count[j] += (j == 0 ? 1 : count[j - 1]);
-            }
-        }
-    }
- 
-    return count[1];
-}
-    
 public:
     long long maximumSubsequenceCount(string text, string pattern) {
-        return max(count(pattern[0] + text, pattern), count(text + pattern[1], pattern));
+        long long min_chars = 0;
+        long long count0 = 0, count1 = 0;
+        
+        for(auto ch: text) {            
+            if(ch == pattern[1]) {
+                min_chars += count0;
+                count1++;
+            }
+            
+            if(ch == pattern[0]) {
+                count0++;
+            }
+        }
+        
+        return min_chars + max(count0, count1);
     }
 };
