9bb1f41118d01a53b960d446cba397a6a0e6b9b4
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-01-31 17:18:00 +0530

    Time: 83 ms (20.75%), Space: 60.1 MB (53.16%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
+        if(n == 1) return new int[] {0};
         
         Stack<Integer> st = new Stack<Integer>();
         st.push(n - 1);
