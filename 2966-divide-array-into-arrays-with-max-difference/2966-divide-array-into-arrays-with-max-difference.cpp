9fa8e775b6b6da8a4a492599bce6c68e4aebb80b
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-01 23:44:00 +0530

    Time: 162 ms (56.94%), Space: 119.5 MB (39.25%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -4,17 +4,11 @@ public:
         sort(begin(nums), end(nums));
         vector<vector<int>> ans;
         
-        for(int i = 0; i <= nums.size() - 3; i += 3) {
-            vector<int> temp;
-            temp.push_back(nums[i]);
-            if(nums[i + 1] - nums[i] <= k) 
-                temp.push_back(nums[i + 1]);
-            else return vector<vector<int>> {};
-            if(nums[i + 2] - nums[i + 1] <= k && nums[i + 2] - nums[i] <= k)
-                temp.push_back(nums[i + 2]);
-            else return vector<vector<int>> {};
+        for(int i = 2; i < nums.size(); i += 3) {
+            if(nums[i] - nums[i - 2] > k)  
+                return vector<vector<int>> {};
             
-            ans.push_back(temp);
+            ans.push_back({nums[i - 2], nums[i - 1], nums[i]});
         }
         
         return ans;
