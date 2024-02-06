217eaa6b64cb82115754ea1feb7813f8f4a94750
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-07 02:01:12 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (13.34%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         int n = secret.length();
-        unordered_map<char, int> mp;
+        unordered_map<char, int> freq;
         
-        int num_bulls = 0, num_cows = 0;
+        int num_bulls = 0;
         for(int i = 0; i < n; i++) {
-            mp[secret[i]]++;
+            freq[secret[i]]++;
             if(secret[i] == guess[i]) {
-                --mp[secret[i]];
+                --freq[secret[i]];
                 ++num_bulls;
             }
         }
 
-         for(int i = 0; i < n; i++) {   
+        int num_cows = 0;
+        for(int i = 0; i < n; i++) {   
             if(secret[i] == guess[i]) continue;
-            if(mp.find(guess[i]) != mp.end() && mp[guess[i]] > 0) {
+            if(freq.find(guess[i]) != freq.end() && freq[guess[i]] > 0) {
+                --freq[guess[i]];
                 ++num_cows;
-                --mp[guess[i]];
             }
         }
         
         string hint = to_string(num_bulls) + 'A' + to_string(num_cows) + 'B';
-        
         return hint;
     }
 };
