cc1070a1514bcbdac474e1b5de3633d721e77817
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-09 22:09:31 +0530

    Time: 655 ms (22.57%), Space: 272.8 MB (16.92%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -2,25 +2,25 @@ class Solution {
     int mod = 1e9 + 7;
     
 private:
-    int countNumWays(int num, int n, int x, vector<vector<int>>& dp) {
-        if(n < 0) return 0;
-        if(n == 0) return 1;
+    int countNumWays(int num, int curr_sum, int n, int x, vector<vector<int>>& dp) {
+        if(curr_sum > n) return 0;
+        if(curr_sum == n) return 1;
         if(num > n) return 0;
         
-        if(dp[num][n] != -1) return dp[num][n];
+        if(dp[num][curr_sum] != -1) return dp[num][curr_sum];
     
-        int not_take = countNumWays(num + 1, n, x, dp);
+        int not_take = countNumWays(num + 1, curr_sum, n, x, dp);
         int take = 0;
-        if(n - pow(num, x) >= 0) {
-            take = countNumWays(num + 1, n - pow(num, x), x, dp);
+        if(curr_sum + pow(num, x) <= n) {
+            take = countNumWays(num + 1, curr_sum + pow(num, x), n, x, dp);
         }
         
-        return dp[num][n] = (take + not_take) % mod;
+        return dp[num][curr_sum] = (take + not_take) % mod;
     }
     
 public:
     int numberOfWays(int n, int x) {
         vector<vector<int>> dp (n + 1, vector<int>(n + 1, -1));
-        return countNumWays(1, n, x, dp);
+        return countNumWays(1, 0, n, x, dp);
     }
 };
