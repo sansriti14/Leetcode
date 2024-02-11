34b5a19e8fac001c11332c980e328cf718ffc5b7
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-11 15:02:24 +0530

    Time: 40 ms (91.72%), Space: 19.5 MB (11.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,8 +1,6 @@
 class Solution {
 private:
     int findMaxCherries(int row, int col1, int col2, int n, int m, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
-        
-        if(row == n || col1 < 0 || col2 < 0 || col1 == m || col2 == m) return -1e9;
         if(row == n - 1) return (col1 == col2) ? grid[row][col1] : grid[row][col1] + grid[row][col2];
         
         if(dp[row][col1][col2] != -1) return dp[row][col1][col2];
@@ -12,8 +10,10 @@ private:
         for(int i = -1; i <= 1; i++) {
             for(int j = -1; j <= 1; j++) {
                 int cherries = ((col1 == col2) ? grid[row][col1] : grid[row][col1] + grid[row][col2]);
-                cherries += findMaxCherries(row + 1, col1 + i, col2 + j, n, m, grid, dp);
-                max_cherries = max(max_cherries, cherries);  
+                if(row + 1 < n && col1 + i >= 0 && col1 + i < m && col2 + j >= 0 && col2 + j < m) {
+                    cherries += findMaxCherries(row + 1, col1 + i, col2 + j, n, m, grid, dp);
+                    max_cherries = max(max_cherries, cherries);  
+                }
             }
         }
         
