2d93ae6932a6c5011a4b62171b99a97125f8a1c9
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-12 17:11:00 +0530

    Time: 70 ms (78.95%), Space: 44.4 MB (11.84%) - LeetHub

--- 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.cpp
+++ 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.cpp
@@ -1,6 +1,10 @@
+using pis = pair<int, string>;
+
 class Solution {
 private:
-    void findFriends(int n, int id, int level, vector<int> adjList[], set<int>& myFriends) {
+    void findFriends(int id, int level, vector<vector<int>>& friends, vector<int>& myFriends) {
+        int n = friends.size();        
+        
         queue<pair<int, int>> q;
         q.push({id, 0});
         
@@ -11,10 +15,10 @@ private:
             auto [person, curr_level] = q.front();
             q.pop();
             
-            if(curr_level == level) myFriends.insert(person);
+            if(curr_level == level) myFriends.push_back(person);
             if(curr_level > level) break;
             
-            for(auto adjPerson: adjList[person]) {
+            for(auto adjPerson: friends[person]) {
                 if(!visited[adjPerson]) {
                     visited[adjPerson] = true;
                     q.push({adjPerson, curr_level + 1});
@@ -25,32 +29,24 @@ private:
     
 public:
     struct comparator {
-        bool operator()(pair<int, string>& a, pair<int, string>& b) {
+        bool operator()(const pis& a, const pis& b) {
         if(a.first != b.first) return a.first > b.first;
         return a.second > b.second;
         }
     };
     
     vector<string> watchedVideosByFriends(vector<vector<string>>& watchedVideos, vector<vector<int>>& friends, int id, int level) {
-        int n = friends.size();
-        vector<int> adjList[n];
-        for(int i = 0; i < n; i++) {
-            adjList[i] = friends[i];
-        }
-        
-        set<int> myFriends;
-        findFriends(n, id, level, adjList, myFriends);
+        vector<int> myFriends;
+        findFriends(id, level, friends, myFriends);
         
         unordered_map<string, int> mp;
-        for(int person = 0; person < n; person++) {
-            if(myFriends.find(person) != myFriends.end()) {
-                for(auto watchedVideo: watchedVideos[person]) {
-                    mp[watchedVideo]++;
-                }
+        for(auto myFriend: myFriends) {
+            for(auto watchedVideo: watchedVideos[myFriend]) {
+                mp[watchedVideo]++;
             }
         }
         
-        priority_queue<pair<int, string>, vector<pair<int, string>>, comparator> pq;
+        priority_queue<pis, vector<pis>, comparator> pq;
         for(auto [video, freq]: mp) {
             pq.push({freq, video});
         }
