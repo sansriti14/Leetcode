8e781c6c40338cef16cf8239ec4ba23467815ac8
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-13 03:10:02 +0530

    Time: 4 ms (80.73%), Space: 8.5 MB (36.10%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,21 @@
 class Solution {
-private:
-    bool checkPalindrome(string& str) {
-        int n = str.length();
-        int i = 0;
-        while(i < n / 2) {
-            if(str[i] != str[n - i - 1]) return false;
-            i++;
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        int i = 0, j = n - 1;
+        while(i < j) {
+            if(isalnum(s[i]) && isalnum(s[j])) {
+                if(tolower(s[i]) != tolower(s[j])) return false;
+                else {
+                    i++;
+                    j--;
+                }
+            }
+            
+            else if(!isalnum(s[i])) i++;
+            else if(!isalnum(s[j])) j--;
         }
         
         return true;
-    }
-    
-public:
-    bool isPalindrome(string s) {
-        string str = "";
-        for(auto ch: s) {
-            if(isalnum(ch)) {
-                str += tolower(ch);
-            }
-        }
-        
-        return checkPalindrome(str);
     }
 };
