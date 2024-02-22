5837f445262ac6428281126409b5e0b00741a24a
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-22 17:46:15 +0530

    Time: 2 ms (99.85%), Space: 53.9 MB (43.36%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(n == 1) return 1;
+        int count[] = new int[n + 1];
+        
+        for(int[] it: trust) {
+            count[it[0]]--;
+            count[it[1]]++;
+        }
+        
+        for(int i = 0; i < n + 1; i++) {
+            if(count[i] == n - 1) return i;
+        }
+        
+        return -1;
+    }
+}
