7ef6d144d7924331773de94b39f21f091610f5f7
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-27 15:49:43 +0530

    Time: 42 ms (16.16%), Space: 30.7 MB (5.08%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,25 @@
-class Solution {
-private:
-    int findMaxProfit(int day, int num_transactions, int canBuy, int k, vector<int>&prices, vector<vector<vector<int>>>& dp) {
-        if(day == prices.size() || num_transactions == k) return 0;
-
-        if(dp[day][num_transactions][canBuy] != -1) return dp[day][num_transactions][canBuy];
-
-        int profit = 0;
-
-        int doNothing = findMaxProfit(day + 1, num_transactions, canBuy, k, prices, dp);
-
-        if(canBuy) {
-            profit += max(doNothing, -prices[day] + findMaxProfit(day + 1, num_transactions, 0, k, prices, dp));
-        }
-
-        else profit += max(doNothing, prices[day] + findMaxProfit(day + 1, num_transactions + 1, 1, k, prices, dp));
-
-        return dp[day][num_transactions][canBuy] = profit;
-    }    
-    
+class Solution {  
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp (n + 1, vector<vector<int>> (k, vector<int> (2, -1)));
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(k + 1, vector<int>(2, 0)));
 
-        return findMaxProfit(0, 0, 1, k, prices, dp);
+        for (int day = n - 1; day >= 0; day--) {
+            for (int num_transactions = 0; num_transactions <= k; num_transactions++) {
+                for (int canBuy = 0; canBuy <= 1; canBuy++) {
+                    int& profit = dp[day][num_transactions][canBuy];
+
+                    int doNothing = dp[day + 1][num_transactions][canBuy];
+
+                    if (canBuy) {
+                        profit = max(doNothing, -prices[day] + dp[day + 1][num_transactions][0]);
+                    } else if (num_transactions < k) {
+                        profit = max(doNothing, prices[day] + dp[day + 1][num_transactions + 1][1]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][0][1];
     }
 };
