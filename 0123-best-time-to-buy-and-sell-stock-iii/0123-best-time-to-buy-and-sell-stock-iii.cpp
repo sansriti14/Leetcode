868b8cecf379de5205d27367fc4dd741211bfcdc
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-02-27 15:47:09 +0530

    Time: 559 ms (15.88%), Space: 275.4 MB (5.08%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,25 @@
 class Solution {
-private:
-    int findMaxProfit(int day, int num_transactions, int canBuy, vector<int>&price, vector<vector<vector<int>>>& dp) {
-        if(day == price.size() || num_transactions == 2) return 0;
+public:
+    int maxProfit(vector<int>& price) {
+        int n = price.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(3, vector<int>(2, 0)));
 
-        if(dp[day][num_transactions][canBuy] != -1) return dp[day][num_transactions][canBuy];
+        for (int day = n - 1; day >= 0; day--) {
+            for (int num_transactions = 0; num_transactions <= 2; num_transactions++) {
+                for (int canBuy = 0; canBuy <= 1; canBuy++) {
+                    int& profit = dp[day][num_transactions][canBuy];
 
-        int profit = 0;
+                    int doNothing = dp[day + 1][num_transactions][canBuy];
 
-        int doNothing = findMaxProfit(day + 1, num_transactions, canBuy, price, dp);
-
-        if(canBuy) {
-            profit += max(doNothing, -price[day] + findMaxProfit(day + 1, num_transactions, 0, price, dp));
+                    if (canBuy) {
+                        profit = max(doNothing, -price[day] + dp[day + 1][num_transactions][0]);
+                    } else if (num_transactions < 2) {
+                        profit = max(doNothing, price[day] + dp[day + 1][num_transactions + 1][1]);
+                    }
+                }
+            }
         }
 
-        else profit += max(doNothing, price[day] + findMaxProfit(day + 1, num_transactions + 1, 1, price, dp));
-
-        return dp[day][num_transactions][canBuy] = profit;
-    }
-    
-public:
-    int maxProfit(vector<int>&price){
-        int n = price.size();
-        vector<vector<vector<int>>> dp (n + 1, vector<vector<int>> (3, vector<int> (2, -1)));
-
-        return findMaxProfit(0, 0, 1, price, dp);
+        return dp[0][0][1];
     }
 };
