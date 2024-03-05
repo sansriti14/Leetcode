32ee05610abd74c8d8ac86c87e6b42734289cca3
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-03-05 19:16:25 +0530

    Time: 26 ms (55.86%), Space: 13.9 MB (44.14%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int minimumLength(string s) {
         int n = s.length();
-        if(n == 1 || s[0] != s[n - 1]) return n;
         
         int low = 0, high = n - 1;
-        while(low < high) {
+        while (low < high) {
             if(s[low] != s[high]) break;
             
             char ch = s[low];
-            while(low <= high && s[low] == ch) low++;
-            
-            while(low <= high && s[high] == ch) high--;
+            while (low <= high && s[low] == ch) low++;
+            while (low <= high && s[high] == ch) high--;
         }
         
         return high - low + 1;
