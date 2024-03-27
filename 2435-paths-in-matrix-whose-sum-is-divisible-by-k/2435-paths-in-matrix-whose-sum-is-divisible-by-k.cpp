ed95ef9fcf5bfdb1cbf4af5c377d6b0de976c5d1
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-03-27 18:56:35 +0530

    Time: 365 ms (12.33%), Space: 137.3 MB (71.00%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,16 +1,19 @@
 class Solution {
+    int m, n;    
+    
 private:
-    int countNumberOfPaths(int currRow, int currCol, int currSum, int m, int n, int k, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
+    int countNumberOfPaths(int currRow, int currCol, int currSum, int k, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
         if(currRow >= m || currCol >= n) return 0;
+        
+        currSum += grid[currRow][currCol];
         if(currRow == m - 1 && currCol == n - 1) {
-            currSum += grid[currRow][currCol];
             return (currSum % k == 0);
         }
         
         if(dp[currRow][currCol][currSum % k] != -1) return dp[currRow][currCol][currSum % k];
         
-        int right = countNumberOfPaths(currRow, currCol + 1, currSum + grid[currRow][currCol], m, n, k, grid, dp);
-        int down = countNumberOfPaths(currRow + 1, currCol, currSum + grid[currRow][currCol], m, n, k, grid, dp);
+        int right = countNumberOfPaths(currRow, currCol + 1, currSum, k, grid, dp);
+        int down = countNumberOfPaths(currRow + 1, currCol, currSum, k, grid, dp);
         
         int mod = 1e9 + 7;
         return dp[currRow][currCol][currSum % k] = (right + down) % mod;
@@ -18,10 +21,10 @@ private:
     
 public:
     int numberOfPaths(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
+        m = grid.size();
+        n = grid[0].size();
         
         vector<vector<vector<int>>> dp (m, vector<vector<int>> (n, vector<int> (k, -1)));
-        return countNumberOfPaths(0, 0, 0, m, n, k, grid, dp);
+        return countNumberOfPaths(0, 0, 0, k, grid, dp);
     }
 };
