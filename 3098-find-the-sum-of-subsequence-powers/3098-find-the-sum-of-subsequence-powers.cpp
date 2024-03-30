d488a6389bb03e0f5495495fab0c535e5939267b
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-03-31 01:56:57 +0530

    Time: 2442 ms (100.00%), Space: 385.7 MB (100.00%) - LeetHub

---
+++ 3098-find-the-sum-of-subsequence-powers/3098-find-the-sum-of-subsequence-powers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    unordered_map<int, unordered_map<int, unordered_map<int, unordered_map<int, int>>>> dp;
+    int mod = 1e9 + 7;
+
+    int findSumOfPowers(int currIndex, int prevIndex, int k, int currDiff, vector<int>& nums) {
+        if (k == 0) return currDiff % mod;
+        if (currIndex == nums.size()) return 0;
+        
+        if (dp[currIndex][prevIndex][k].count(currDiff))
+            return dp[currIndex][prevIndex][k][currDiff];
+
+        int take = 0;
+        if (prevIndex == -1)
+            take = findSumOfPowers(currIndex + 1, currIndex, k - 1, currDiff, nums);
+        else
+            take = findSumOfPowers(currIndex + 1, currIndex, k - 1, min(currDiff, abs(nums[currIndex] - nums[prevIndex])), nums);
+        int notTake = findSumOfPowers(currIndex + 1, prevIndex, k, currDiff, nums);
+
+        return dp[currIndex][prevIndex][k][currDiff] = (take % mod + notTake % mod) % mod;
+    }
+
+public:
+    int sumOfPowers(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return findSumOfPowers(0, -1, k, 1e9, nums);
+    }
+};
+
