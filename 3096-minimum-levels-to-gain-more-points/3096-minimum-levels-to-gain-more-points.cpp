3c98b95cee9ad261d23b8605af884b552643aa27
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-03-31 13:02:04 +0530

    Time: 200 ms (100.00%), Space: 260.3 MB (100.00%) - LeetHub

--- 3096-minimum-levels-to-gain-more-points/3096-minimum-levels-to-gain-more-points.cpp
+++ 3096-minimum-levels-to-gain-more-points/3096-minimum-levels-to-gain-more-points.cpp
@@ -1,19 +1,20 @@
 class Solution {
-    public int minimumLevels(int[] possible) {
-        int n = possible.length;
+public:
+    int minimumLevels(vector<int>& possible) {
+        int n = possible.size();
         
-        int totalSum = 0;
-        for(int i = 0; i < n; i++) {
-            if(possible[i] == 0) possible[i] = -1;
-            totalSum += possible[i];
-        }
+        for (auto &it: possible) 
+            if (it == 0) it = -1;
         
-        int currScore = 0;
-        for(int i = 0; i < n - 1; i++) {
-            currScore += possible[i];
-            if(totalSum - currScore < currScore) return i + 1;
+        int totalSum = accumulate(begin(possible), end(possible), 0);
+        
+        int ans = 0;
+        int curr_score = 0;
+        for(int i = 0; i < possible.size() - 1; i++) {
+            curr_score += possible[i];
+            if(totalSum - curr_score < curr_score) return i + 1;
         }
         
         return -1;
     }
-}
+};
