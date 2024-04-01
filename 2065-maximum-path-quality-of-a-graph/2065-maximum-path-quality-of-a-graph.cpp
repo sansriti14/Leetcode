8a4c9d2ff2a9f68ebb55282cb7846dc6f4e7291b
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-04-01 19:27:43 +0530

    Time: 271 ms (21.54%), Space: 25.2 MB (46.67%) - LeetHub

--- 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.cpp
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.cpp
@@ -1,25 +1,4 @@
 class Solution {
-private:
-    void dfs(vector<int>& values, int currNode, int currTime, int maxTime, int currQuality, int& maxQuality, vector<pair<int, int>> adjList[], vector<int>& visited) {
-        if (currTime > maxTime) return;
-
-        if (visited[currNode] == 0) currQuality += values[currNode];
-
-        visited[currNode]++;
-
-        if (currNode == 0) {
-            maxQuality = max(maxQuality, currQuality);
-        }
-
-        for (auto it: adjList[currNode]) {
-            int adjNode = it.first;
-            int newTime = currTime + it.second; 
-            dfs(values, adjNode, newTime, maxTime, currQuality, maxQuality, adjList, visited);
-        }
-
-        visited[currNode]--;
-    }
-    
 public:
     int maximalPathQuality(vector<int>& values, vector<vector<int>>& edges, int maxTime) {
         int n = values.size();
@@ -31,8 +10,29 @@ public:
 
         int maxQuality = 0;
         vector<int> visited(n, 0);
-        dfs(values, 0, 0, maxTime, 0, maxQuality, adjList, visited);
+        visited[0] = 1;
         
+        function<void(int, int, int)> dfs = [&] (int currNode, int currTime, int currQuality) {
+            if (currTime > maxTime) return;
+
+            if (visited[currNode] == 0) currQuality += values[currNode];
+
+            visited[currNode]++;
+
+            if (currNode == 0) {
+                maxQuality = max(maxQuality, currQuality);
+            }
+
+            for (auto it: adjList[currNode]) {
+                int adjNode = it.first;
+                int newTime = currTime + it.second; 
+                dfs(adjNode, newTime, currQuality);
+            }
+
+            visited[currNode]--;
+        };
+        
+        dfs(0, 0, values[0]);
         return maxQuality;
     }
 };
