9561c41ce6622734c29b529d6795b6c5d1d3c85a
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-04-07 14:20:03 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (92.84%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int unmatchedLeft = 0;  
-        int maxRightNeeded = 0; 
+        int n = s.length();
         
-        for (char ch : s) {
-            if (ch == '(')
-                unmatchedLeft++, maxRightNeeded++;
-            if (ch == ')')
-                maxRightNeeded--, unmatchedLeft = max(unmatchedLeft - 1, 0); 
-            if (ch == '*')
-                maxRightNeeded++, unmatchedLeft = max(unmatchedLeft - 1, 0);
-            if (maxRightNeeded < 0) return false;
+        int countOpened = 0;
+        int countAsterisk = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '*') countAsterisk++;
+            
+            else if (s[i] == '(') countOpened++;
+            
+            else if (s[i] == ')') {
+                if (countOpened > 0) countOpened--;
+                else if (countAsterisk > 0) countAsterisk--;
+                else return false;
+            }
         }
         
-        return (unmatchedLeft == 0);
+        int countClosed = 0;
+        countAsterisk = 0;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '*') countAsterisk++;
+            
+            else if (s[i] == ')') countClosed++;
+            
+            else if (s[i] == '(') {
+                if (countClosed > 0) countClosed--;
+                else if (countAsterisk > 0) countAsterisk--;
+                else return false;
+            }
+        }
+        
+        return true;
     }
 };
