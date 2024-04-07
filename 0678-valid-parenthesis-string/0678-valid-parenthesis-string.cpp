142bd7b62833e28eef3f0d45afa89b1c24f62ab3
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-04-07 14:02:55 +0530

    Time: 3 ms (45.50%), Space: 7.5 MB (39.07%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -7,14 +7,20 @@ public:
         int countAsterisk = 0;
         
         for (int i = 0; i < n; i++) {
-            if (s[i] == '*') countAsterisk++;
-            
-            else if (s[i] == '(') st.push(s[i]);
-            
-            else if (s[i] == ')') {
-                if (!st.empty()) st.pop();
-                else if (countAsterisk > 0) countAsterisk--;
-                else return false;
+            switch (s[i]) {
+                case '*':
+                    countAsterisk++;
+                    break;
+                
+                case '(':
+                    st.push(s[i]);
+                    break;
+                
+                case ')':
+                    if (!st.empty()) st.pop();
+                    else if (countAsterisk > 0) countAsterisk--;
+                    else return false;  
+                    break;
             }
         }
         
@@ -22,14 +28,20 @@ public:
         countAsterisk = 0;
         
         for (int i = n - 1; i >= 0; i--) {
-            if (s[i] == '*') countAsterisk++;
-            
-            else if (s[i] == ')') st.push(s[i]);
-            
-            else if (s[i] == '(') {
-                if (!st.empty()) st.pop();
-                else if (countAsterisk > 0) countAsterisk--;
-                else return false;
+            switch (s[i]) {
+                case '*':
+                    countAsterisk++;
+                    break;
+                
+                case ')':
+                    st.push(s[i]);
+                    break;
+                
+                case '(':
+                    if (!st.empty()) st.pop();
+                    else if (countAsterisk > 0) countAsterisk--;
+                    else return false;  
+                    break;
             }
         }
         
