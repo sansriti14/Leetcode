7defeb361ee0d865682f0d6eba5a9efdab8896e7
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-04-20 12:54:50 +0530

    Time: 104 ms (67.64%), Space: 70.5 MB (68.73%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,23 +1,51 @@
 class Solution {
 public:
+    
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    
+    bool isValid(int r, int c, int n, int m)
+    {
+        if(r>=0 && r<n && c>=0 && c<m)
+            return true;
+        return false;
+    }
+    
+    void dfsHelper(int r, int c, vector<vector<int>>&land, int &r1, int &c1, int &r2, int &c2){
+        land[r][c] = 0;
+        
+        for(int i=0; i<4; i++)
+        {
+            int nr = r + dx[i];
+            int nc = c + dy[i];
+            
+            if(isValid(nr, nc, land.size(), land[0].size()) && land[nr][nc]==1)
+            {
+                r1 = min(r1, nr);
+                c1 = min(c1, nc);
+                r2 = max(r2, nr);
+                c2 = max(c2, nc);
+                dfsHelper(nr, nc, land, r1, c1, r2, c2);
+            }
+            
+        }
+    }
+    
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        int m = land.size();
-        int n = land[0].size();
-        
         vector<vector<int>> ans;
+        int i, j;
+        int n = land.size(), m = land[0].size();
         
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (land[r][c] == 0) continue;
-                
-                int x = r, y = c;
-                for (x = r; x < m && land[x][c]; x++) {
-                    for (y = c; y < n && land[x][y]; y++) {
-                        land[x][y] = 0;
-                    }
-                } 
-                
-                ans.push_back({r, c, x - 1, y - 1});
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<m; j++)
+            {
+                if(land[i][j])
+                {
+                    int r1 = i, c1 = j, r2 = i, c2 = j;
+                    dfsHelper(i, j, land, r1, c1, r2, c2);
+                    ans.push_back({r1, c1, r2, c2});
+                }
             }
         }
         
