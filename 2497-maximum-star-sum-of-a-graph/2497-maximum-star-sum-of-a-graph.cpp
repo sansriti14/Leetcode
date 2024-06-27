ebb2ab92273a35f14876cd96c661949dbfecbfc7
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-06-27 21:52:33 +0530

    Time: 248 ms (72.14%), Space: 129.7 MB (72.14%) - LeetHub

--- 2497-maximum-star-sum-of-a-graph/2497-maximum-star-sum-of-a-graph.cpp
+++ 2497-maximum-star-sum-of-a-graph/2497-maximum-star-sum-of-a-graph.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
         int n = vals.size();
-        vector<vector<int>> adjList(n);
         
-        for (auto& edge : edges) {
-            adjList[edge[0]].push_back(vals[edge[1]]);
-            adjList[edge[1]].push_back(vals[edge[0]]);
+        vector<int> adjList[n];
+        for (auto it: edges) {
+            adjList[it[0]].push_back(vals[it[1]]);
+            adjList[it[1]].push_back(vals[it[0]]);
         }
         
         int maxStarSum = INT_MIN;
         
         for (int node = 0; node < n; node++) {
-            sort(begin(adjList[node]), end(adjList[node]), greater<int>()); 
+            sort(begin(adjList[node]), end(adjList[node]), greater<int>());
             
             int currStarSum = vals[node];
-            
-            for (int i = 0; i < min(k, (int)adjList[node].size()); i++) {
-                if (adjList[node][i] > 0) {
-                    currStarSum += adjList[node][i];
-                } 
                 
-                else break;
+            for (int i = 0; i < min(k, (int)adjList[node].size()); i++) {
+                if (adjList[node][i] < 0) break;
+
+                currStarSum += adjList[node][i];
             }
-            
+
             maxStarSum = max(maxStarSum, currStarSum);
+            
         }
         
         return maxStarSum;
