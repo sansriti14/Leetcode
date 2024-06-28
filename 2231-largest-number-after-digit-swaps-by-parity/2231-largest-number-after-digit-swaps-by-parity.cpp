9c8f0660b12788469e5bb5a35a0fa83e68067c46
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-06-28 21:04:57 +0530

    Time: 4 ms (14.96%), Space: 7.4 MB (68.08%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int largestInteger(int num) {
         string str = to_string(num);
-        vector<int> even_ele, odd_ele;
+        priority_queue<int> even_ele, odd_ele;
         
         for (auto &ch: str) {
+            if ((ch - '0') % 2) odd_ele.push(ch);
+            else even_ele.push(ch);
+            
+        }
+
+        for (auto &ch: str) {
             if ((ch - '0') % 2) {
-                odd_ele.push_back(ch);
+                ch = odd_ele.top();
+                odd_ele.pop();
             }
             
             else {
-                even_ele.push_back(ch);
+                ch = even_ele.top();
+                even_ele.pop();
             }
-        }
-        
-        sort(even_ele.rbegin(), even_ele.rend());
-        sort(odd_ele.rbegin(), odd_ele.rend());
-        
-        int even_index = 0, odd_index = 0;
-        
-        for (int i = 0; i < str.length(); i++) {
-            str[i] = ((str[i] - '0') % 2) ? odd_ele[odd_index++] : even_ele[even_index++];
         }
         
         return stoi(str);
