48ba99cafc571a9288ad1d6033e67a94c7a07816
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-06-28 21:01:30 +0530

    Time: 4 ms (14.96%), Space: 8.3 MB (5.74%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -2,34 +2,25 @@ class Solution {
 public:
     int largestInteger(int num) {
         string str = to_string(num);
-        
-        vector<int> even_indices, odd_indices;
         vector<int> even_ele, odd_ele;
         
-        for (int i = 0; i < str.length(); i++) {
-            if ((str[i] - '0') % 2) {
-                odd_indices.push_back(i);
-                odd_ele.push_back(str[i]);
+        for (auto &ch: str) {
+            if ((ch - '0') % 2) {
+                odd_ele.push_back(ch);
             }
             
             else {
-                even_indices.push_back(i);
-                even_ele.push_back(str[i]);
+                even_ele.push_back(ch);
             }
         }
         
-        sort(odd_indices.begin(), odd_indices.end());
-        sort(even_indices.begin(), even_indices.end());
         sort(even_ele.rbegin(), even_ele.rend());
         sort(odd_ele.rbegin(), odd_ele.rend());
         
-        str.clear();
-        for (int i = 0; i < odd_indices.size(); i++) {
-            str[odd_indices[i]] = odd_ele[i];
-        }
+        int even_index = 0, odd_index = 0;
         
-        for (int i = 0; i < even_indices.size(); i++) {
-            str[even_indices[i]] = even_ele[i];
+        for (int i = 0; i < str.length(); i++) {
+            str[i] = ((str[i] - '0') % 2) ? odd_ele[odd_index++] : even_ele[even_index++];
         }
         
         return stoi(str);
