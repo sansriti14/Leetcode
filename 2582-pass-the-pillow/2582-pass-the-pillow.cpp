1edc4994a6c4a7b7b0aabe3dcac839e6bb1d3a09
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-07-06 23:45:01 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (71.18%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        if (time < n) return time + 1;
-        
-        return ((time / (n - 1)) % 2 == 0) ? 1 + time % (n - 1) : n - (time % (n - 1));
+        n = n - 1;
+        return ((time / n) % 2) ? 1 + n - (time % n) : 1 + (time % n);
     }
 };
