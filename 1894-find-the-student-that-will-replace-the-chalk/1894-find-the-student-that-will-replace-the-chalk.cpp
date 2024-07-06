8e793a4dc4502ee2300c0880c1390b646d6fde11
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-07-07 00:03:59 +0530

    Time: 94 ms (67.84%), Space: 76.6 MB (81.58%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        int n = chalk.size();
-        
         long long inOneRound = accumulate(begin(chalk), end(chalk), (long long)0);
-        int m = k / inOneRound;
-        k -= (inOneRound * m);
+        
+        int numRounds = k / inOneRound;
+        k -= (inOneRound * numRounds);
         
         int i = 0;
         while (k >= chalk[i]) {
-            k -= chalk[i];
-            i++;
+            k -= chalk[i++];
         }
         
         return i;
