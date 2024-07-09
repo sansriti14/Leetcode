225d23f8feb35fc4c1024480b097c0b73c59051a
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-07-09 21:02:35 +0530

    Time: 146 ms (67.94%), Space: 76.5 MB (57.65%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.cpp
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -4,15 +4,15 @@ public:
         int n = customers.size();
         
         int prev_finish_time = customers[0][0] + customers[0][1];
-        long long total_wt = customers[0][1];
+        long long total_wait = customers[0][1];
         
         for (int i = 1; i < n; i++) {
-            total_wt += customers[i][1];
-            total_wt += max(0, prev_finish_time - customers[i][0]);
+            total_wait += customers[i][1];
+            total_wait += max(0, prev_finish_time - customers[i][0]);
             
             prev_finish_time = max(prev_finish_time, customers[i][0]) + customers[i][1];
         }
         
-        return (double)total_wt / n;
+        return (double)total_wait / n;
     }
 };
