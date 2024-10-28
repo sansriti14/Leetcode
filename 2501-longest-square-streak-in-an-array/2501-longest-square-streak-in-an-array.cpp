6837d2a9d35606d6e95ff508b33af9efa66f4c3b
Sansriti Maurya <81472811+sansriti14@users.noreply.github.com>
2024-10-28 19:11:12 +0530

    Time: 123 ms (28.89%), Space: 109 MB (52.73%) - LeetHub

---
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestSquareStreak(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        
+        unordered_map<int, int> mp;
+        int maxLength = -1;
+        
+        for (auto it: nums) {
+            int root = sqrt(it);
+            if (root * root == it) {
+                mp[it] = 1 + mp[root];
+            }
+            
+            else mp[it] = 1;
+            
+            maxLength = max(maxLength, mp[it]);
+        }
+        
+        return (maxLength < 2) ? -1 : maxLength;
+    }
+};
